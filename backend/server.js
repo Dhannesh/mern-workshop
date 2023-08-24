a07b4cf24ff0234e3c3e425743a8c02cc19c4c92
Dhaneshwar Kumar <dhannesh@gmail.com>
2023-08-24 12:19:29 +0530

    db api completed

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import colors from "colors";
 import dotenv from "dotenv";
 import getRoutes from "./routes/getRoutes.js";
 import { errorHandler } from "./middleware/errorMiddleware.js";
+import { connectDB } from "./config/db.js";
 dotenv.config();
+connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
